9eace1868cfbaae6c51d61e43e335f6274b30c39
Programming Hero <programming.hero1@gmail.com>
2023-04-03 16:53:22 -0500

    initial data load

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h2>Order Review Page</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Main from './components/Layout/Main';
+import OrderReview from './components/OrderReview/OrderReview';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('tshirts.json')
+      },
+      {
+        path: 'review',
+        element: <OrderReview></OrderReview>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
